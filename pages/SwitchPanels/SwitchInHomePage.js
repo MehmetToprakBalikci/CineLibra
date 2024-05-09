4656d6418e22ad04ac4bd0b0b44fa5580fc4bd57
Mehmet Toprak Balıkcı <149236560+MehmetToprakBalikci@users.noreply.github.com>
2024-05-09 13:03:21 +0300

    Minor visual update

--- pages/SwitchPanels/SwitchInHomePage.js
+++ pages/SwitchPanels/SwitchInHomePage.js
@@ -1,6 +1,7 @@
 // SwitchPanel.js
 import React from 'react';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import {colors} from '../../components/colorProfile';
 import Profile from '../../screens/Profile';
 import SignOutScreen from '../../screens/SignOutScreen';
 import CustomDrawer from '../../components/CustomDrawer';
@@ -10,51 +11,52 @@ import SearchPage from '../SearchPage';
 import StartPage from '../StartPage';
 const Drawer = createDrawerNavigator();
 
+const accent_color_strong = colors.accent_strong
 
 const SwitchHomePanel = () => {
   return (
-    <Drawer.Navigator   drawerContent={props=> <CustomDrawer {...props}  />} 
+    <Drawer.Navigator   drawerContent={props=> <CustomDrawer {...props}  />}
      screenOptions={{
         headerShown: false,
         swipeEnabled: false,
-       
+
       }}
     >
 
 <Drawer.Screen name="Search" component={HomePage}  options={{
        drawerIcon: ({color})=> (
-        <Ionicons name= "search-outline" size={20} color={'#6A5ACD'}/>
+        <Ionicons name= "search-outline" size={20} color={accent_color_strong}/>
        ),
           drawerLabelStyle: {
             color: 'white', // Set the color of the label
-           
-            
+
+
           },
     }} />
 
 
     <Drawer.Screen name="Profile" component={Profile}  options={{
        drawerIcon: ({color})=> (
-        <Ionicons name= "person-outline" size={20} color={'#6A5ACD'}/>
+        <Ionicons name= "person-outline" size={20} color={accent_color_strong}/>
        ),
           drawerLabelStyle: {
             color: 'white', // Set the color of the label
-           
-            
+
+
           },
       }} />
 
 
      <Drawer.Screen name="SignOut" component={SignOutScreen}  options={{
        drawerIcon: ({color})=> (
-        <Ionicons name="log-out" size={20} color={'#6A5ACD'} />
+        <Ionicons name="log-out" size={20} color={accent_color_strong} />
        ),
         drawerLabelStyle: {
             color: 'white', // Set the color of the label
-           
-            
+
+
           },
-    
+
       }}/>
     </Drawer.Navigator>
   );
