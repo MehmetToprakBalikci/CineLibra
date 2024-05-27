1e8c8727ea4001035e9a865b3979c3cf92720371
Mehmet Toprak Balıkcı <149236560+MehmetToprakBalikci@users.noreply.github.com>
2024-05-27 18:02:22 +0300

    Logout Fixes

--- pages/BookPages/BookHomePage.js
+++ pages/BookPages/BookHomePage.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-import {StyleSheet, View, Text, ImageBackground, SafeAreaView} from "react-native";
+import {StyleSheet, View, Text, ImageBackground, SafeAreaView, Platform, StatusBar} from "react-native";
 import BookSearchBar from "../../components/BookPageComponents/BookSearchBar";
 import Lists from "../../components/MoviePageComponents/Lists";
 import LeftBar from "../../components/MoviePageComponents/LeftBar";
@@ -64,5 +64,6 @@ const styles = StyleSheet.create({
     safeArea: {
         flex:1,
         backgroundColor:bg_filter_color,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight+20 : 0,
     }
 });
