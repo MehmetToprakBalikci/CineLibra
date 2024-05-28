b38ce7b19796632ea100786f324032ce0f610589
Mehmet Toprak Balıkcı <149236560+MehmetToprakBalikci@users.noreply.github.com>
2024-05-28 16:29:45 +0300

    Profile page dark mode issue fixed

--- screens/Profile.js
+++ screens/Profile.js
@@ -10,6 +10,8 @@ import {colors} from '../components/MoviePageComponents/colorProfile';
 
 const text_color = colors.text_color
 const text_color_weak = colors.text_color_weak
+const bg_filter_color = colors.bg_filter_color
+const bg_filter_color_strong = colors.bg_filter_color_strong
 
 export default function Profile() {
 
@@ -17,6 +19,7 @@ export default function Profile() {
 
     return (
         <ImageBackground style={{flex:1}} blurRadius={200} source={require('../assets/bg_alt.jpg')}>
+            <View style={{backgroundColor:bg_filter_color, flex:1}}>
             <SafeAreaView style={styles.safeAreaStyle}>
                 <TouchableOpacity  onPress={()=> navigation.goBack()}>
                     <AntDesign name="arrowleft" size={35} style={styles.arrowLeft} />
@@ -33,6 +36,7 @@ export default function Profile() {
 
 
             </SafeAreaView>
+            </View>
         </ImageBackground>
     )
 }
